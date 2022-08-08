c55e1db88f0de8c140b85ae47e17ae3ef6e3994c
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-08-08 17:31:50 +0100

    working on crew list page and crew detail box

--- components/CrewDetailBox.js
+++ components/CrewDetailBox.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Link from "next/link";
 
 const listOfCrew = [
   {
+    id: 1,
     name: "John Doe",
     role: "Director of Photography",
     image:
@@ -11,6 +13,7 @@ const listOfCrew = [
     phone: "",
   },
   {
+    id: 2,
     name: "Jane Doe",
     role: "Camera Operator",
     image:
@@ -20,6 +23,7 @@ const listOfCrew = [
     phone: "",
   },
   {
+    id: 3,
     name: "Jack Doe",
     role: "Camera Operator Steadicam",
     image:
@@ -28,22 +32,71 @@ const listOfCrew = [
     email: "",
     phone: "",
   },
+  {
+    id: 4,
+    name: "Jill Doe",
+    role: "Focus Puller",
+    image:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    bio: "Jill Doe is a focus puller. She is a very good focus puller.",
+    email: "",
+    phone: "",
+  },
+  {
+    id: 5,
+    name: "Joe Doe",
+    role: "Loader",
+    image:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    bio: "Joe Doe is a loader. He is a very good loader.",
+    email: "",
+    phone: "",
+  },
+  {
+    id: 6,
+    name: "Juan Doe",
+    role: "Trainee",
+    image:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    bio: "Juan Doe is a trainee. He is a very good trainee.",
+    email: "",
+    phone: "",
+  },
 ];
 
 const CrewDetailBox = (crew) => {
   return listOfCrew.map((crew, i) => {
+    const slugify = (str) =>
+      str
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, "")
+        .replace(/[\s_-]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+
     const styling = {
       backgroundImage: `url(${crew.image})`,
-      width:"100px",
-      height:"100px",
-      backgroundSize:"cover",
-  }
+      width: "100px",
+      height: "100px",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    };
+
     return (
-      <div key={crew + i} className="p-4 gap-x-4 bg-white rounded shadow-md">
-        <div  style={styling}
-          className="rounded-full overflow-hidden w-[100px] h-[100px] flex items-center justify-center">
-        </div>
-      </div>
+      <Link
+        key={crew + i}
+        href={`/I-need-crew/crew-list/department/${slugify(crew.name)}${crew.id}`}
+      >
+        <a className="p-4 flex items-center border-b border-wearecrewBlue gap-x-4 bg-white rounded shadow-md cursor-pointer">
+          <div
+            style={styling}
+            className="rounded-full overflow-hidden w-[100px] h-[100px] flex items-center justify-center"
+          ></div>
+          <div className="">
+            <h2>{crew.name}</h2>
+          </div>
+        </a>
+      </Link>
     );
   });
 };
--- pages/I-need-crew/depts/crew-list/[crewList].js
+++ pages/I-need-crew/depts/crew-list/[crewList].js
@@ -4,19 +4,19 @@ import CrewDetailBox from '../../../../components/CrewDetailBox';
 const crewList = () => {
   return (
     <div className="flex px-12 gap-x-4 pt-12">
-      <div className="w-2/3 borderRed flex flex-col gap-y-4">
+      <div className="w-2/3 flex flex-col gap-y-4 overflow-scroll max-h-[calc(100vh-200px)]">
         <CrewDetailBox/>
       </div>
       
-      <div className="w-1/3 shadow-md bg-white border-b-wearecrewBlue rounded-md p-4">
+      <div className="w-1/3 shadow-md bg-white border-b-wearecrewBlue rounded-md p-4 h-full">
         <p className="text-center mb-4 font-semibold text-lg">Filter by Role</p>
         <ul className="flex flex-col gap-y-4 filterByList">
-          <li>Director of Photography</li>
-          <li>Camera Operator</li>
-          <li>Camera Operator Steadicam</li>
-          <li>Focus Puller</li>
-          <li>Loader</li>
-          <li>Trainee</li>
+          <li><button className="w-full">Director of Photography</button></li>
+          <li><button className="w-full">Camera Operator</button></li>
+          <li><button className="w-full">Camera Operator / Steadicam</button></li>
+          <li><button className="w-full">Focus Puller</button></li>
+          <li><button className="w-full">Loader</button></li>
+          <li><button className="w-full">Trainee</button></li>
         </ul>
       </div>
 
