b9ffb6388a571dddf568f9e9f39981a2929d0fa2
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-08-17 17:28:00 +0100

    added more depts

--- components/FilteredDept.js
+++ components/FilteredDept.js
@@ -6,7 +6,7 @@ const deptList = [
   { dept: "Animals", imgUrl: "../../../images/icons/animals.png" },
   { dept: "Armoury", imgUrl: "../../../images/icons/armoury.png" },
   { dept: "Art", imgUrl: "../../../images/icons/art.png" },
-  { dept: "ADs", imgUrl: "../../../images/icons/ads.png" },
+  { dept: "Assistant Directors", imgUrl: "../../../images/icons/ads.png" },
   { dept: "Camera", imgUrl: "../../../images/icons/camera.png" },
   { dept: "Casting", imgUrl: "../../../images/icons/casting.png" },
   { dept: "Catering", imgUrl: "../../../images/icons/catering.png" },
@@ -16,6 +16,9 @@ const deptList = [
   { dept: "Costume", imgUrl: "../../../images/icons/costume.png" },
   { dept: "Covid", imgUrl: "../../../images/icons/covid.png" },
   { dept: "Directors", imgUrl: "../../../images/icons/directors.png" },
+  { dept: "Dialogue", imgUrl: "../../../images/icons/dialogue.png" },
+  { dept: "D.I.T", imgUrl: "../../../images/icons/dit.png" },
+  { dept: "Drone", imgUrl: "../../../images/icons/drone.png" },
   { dept: "Editoral & Post", imgUrl: "../../../images/icons/editorial.png" },
   { dept: "Electrical", imgUrl: "../../../images/icons/electrical.png" },
   { dept: "Grip", imgUrl: "../../../images/icons/grip.png" },
@@ -23,6 +26,7 @@ const deptList = [
   { dept: "Health & Safety", imgUrl: "../../../images/icons/healthSafety.png" },
   { dept: "HR", imgUrl: "../../../images/icons/hr.png" },
   { dept: "Locations", imgUrl: "../../../images/icons/locations.png" },
+  { dept: "Medical", imgUrl: "../../../images/icons/medical.png" },
   { dept: "Producers", imgUrl: "../../../images/icons/producer.png" },
   { dept: "Production Crew", imgUrl: "../../../images/icons/production.png" },
   { dept: "Props", imgUrl: "../../../images/icons/props.png" },
@@ -30,15 +34,16 @@ const deptList = [
   { dept: "Set Decoration", imgUrl: "../../../images/icons/setDec.png" },
   { dept: "Sound", imgUrl: "../../../images/icons/sound.png" },
   { dept: "Special Effects", imgUrl: "../../../images/icons/sfx.png" },
-  { dept: "Stand-bys", imgUrl: "../../../images/icons/sfx.png" },
-  { dept: "Stunts", imgUrl: "../../../images/icons/sound.png" },
-  { dept: "Tracking Vehicles", imgUrl: "../../../images/icons/sfx.png" },
+  { dept: "Stand-bys", imgUrl: "../../../images/icons/standby.png" },
+  { dept: "Stills", imgUrl: "../../../images/icons/stills.png" },
+  { dept: "Stunts", imgUrl: "../../../images/icons/stunts.png" },
+  { dept: "Tracking Vehicles", imgUrl: "../../../images/icons/trackingVeh.png" },
   { dept: "Transport", imgUrl: "../../../images/icons/transport.png" },
+  { dept: "Video", imgUrl: "../../../images/icons/video.png" },
   { dept: "Visual Effects", imgUrl: "../../../images/icons/vfx.png" },
 ];
 
 const FilteredDept = () => {
-  const [name, setName] = useState("");
   const [foundDept, setFoundDept] = useState(deptList);
 
   const filter = (e) => {
