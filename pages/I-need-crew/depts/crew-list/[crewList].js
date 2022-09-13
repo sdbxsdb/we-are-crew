0a0a909156233a49a652733b696b1e69cd0f38f8
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-09-13 12:20:44 +0100

    filtering users as per title working

--- pages/I-need-crew/depts/crew-list/[crewList].js
+++ pages/I-need-crew/depts/crew-list/[crewList].js
@@ -4,205 +4,28 @@ import Link from "next/link";
 import DeptTitle from "../../../../components/DeptTitle";
 import { supabase } from "../../../../utils/supabaseClient";
 
+const CrewList = ({ users }) => {
+  const [foundTitle, setFoundTitle] = useState(users);
 
-// const listOfCrew = [
-//   {
-//     id: 1,
-//     name: "John Doe",
-//     dept: "Acting",
-//     role: "Director of Photography",
-//     status: "Avail",
-//     canStepUp: true,
-//     image:
-//       "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//     bio: "John Doe is a director of photography. He is a very good director.",
-//     email: "johndoe@test.com",
-//     phone: "34563456456",
-//     status: "Available",
-//     willWorkIn: [
-//       "London",
-//       "Liverpool",
-//       "Newcastle upon Tyne",
-//       "Birmingham",
-//       "Exeter",
-//       "Norwich",
-//       "Glasgow",
-//       "Edinbrugh",
-//       "Aberdeen",
-//       "Belfast",
-//       "Derry / L'Derry",
-//       "Enniskillen",
-//       "Dublin",
-//       "Galway",
-//       "Cork",
-//       "Donegal",
-//       "Outside the UK & Ireland",
-//     ],
-//     qualis: "",
-//     credits: [
-//       {
-//         jobTitle: "America Physco",
-//         role: "Director of Photography",
-//       },
-//       {
-//         jobTitle: "Jumanji",
-//         role: "Director of Photography",
-//       },
-//     ],
-//     bio: "Jill Doe is a focus puller. She is a very good focus puller.",
-//     cv: "/images/logoNew2.png",
-//   },
-//   {
-//     id: 2,
-//     name: "Jane Doe",
-//     role: "Camera Operator",
-//     status: "NotAvail",
-//     canStepUp: true,
-//     image: "",
-//     bio: "Jane Doe is a camera operator. She is a very good camera operator.",
-//     email: "janedoe@test.com",
-//     phone: "754674567456",
-//     status: "Not Available",
-//     willWorkIn: ["England", "Northern Ireland", "Ireland"],
-//     qualis: "",
-//     credits: [
-//       {
-//         jobTitle: "A Movie Title",
-//         role: "A Camera Operator",
-//       },
-//       {
-//         jobTitle: "Harry Potter 5",
-//         role: "B Camera Operator",
-//       },
-//     ],
-//     bio: "Jill Doe is a focus puller. She is a very good focus puller.",
-//     cv: "/images/logoNew2.png",
-//   },
-//   {
-//     id: 3,
-//     name: "Jack Doe",
-//     dept: "ADs",
-//     role: "Camera Operator Steadicam",
-//     status: "SemiAvail",
-//     canStepUp: false,
-//     image:
-//       "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//     bio: "Jack Doe is a camera operator. He is a very good camera operator.",
-//     email: "jackdoe@test.com",
-//     phone: "12341234645",
-//     status: "On Dalies",
-//     willWorkIn: ["England"],
-//     qualis: "Steadicam Course",
-//     credits: [
-//       {
-//         jobTitle: "Mission Impossible",
-//         role: "B Camera Operator",
-//       },
-//       {
-//         jobTitle: "Harry Potter 7",
-//         role: "B Camera Operator",
-//       },
-//     ],
-//     bio: "Jack Doe is a camera operator. He is a very good camera operator.",
-//     cv: "",
-//   },
-//   {
-//     id: 4,
-//     name: "Jill Doe",
-//     dept: "Art",
-//     role: "Focus Puller",
-//     status: "Avail",
-//     canStepUp: true,
-//     image:
-//       "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//     bio: "Jill Doe is a focus puller. She is a very good focus puller.",
-//     email: "jilldoe@test.com",
-//     phone: "234234647432345",
-//     status: "On Dalies",
-//     willWorkIn: ["England", "Northern Ireland", "Scotland", "Wales", "Ireland"],
-//     qualis: "Focus Puller Course - Focus Puller Course Grade 2",
-//     credits: [
-//       {
-//         jobTitle: "Fight Club",
-//         role: "B Camera Focus Puller",
-//       },
-//       {
-//         jobTitle: "Harry Potter 7",
-//         role: "B Camera Focus Puller",
-//       },
-//       {
-//         jobTitle: "Harry Potter 7",
-//         role: "B Camera Focus Puller",
-//       },
-//       {
-//         jobTitle: "Harry Potter 7",
-//         role: "B Camera Focus Puller",
-//       },
-//       {
-//         jobTitle: "Harry Potter 7",
-//         role: "B Camera Focus Puller",
-//       },
-//     ],
-//     bio: "Jill Doe fhfjdjkjs f dsaksdhklfha sdfk akjsfd kljasdfkjas dfkj  askfjsakjdfhklasdhf kjasfkljashdfkjahdf a kajsfkaskdjfha sdflkasjfkjasdfh asfkla skljfalksdfhkjashfd klasfdh alskf lkashfkjashfk jashfklashdfkljashdfkjashdf kjsdfhalksdfhsadkfsk",
-//     cv: "/images/logoNew2.png",
-//   },
-//   {
-//     id: 5,
-//     name: "Mynameis Verylongsoitis",
-//     dept: "Camera",
-//     role: "Loader",
-//     status: "NotAvail",
-//     canStepUp: true,
-//     image:
-//       "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//     bio: "Joe Doe is a loader. He is a very good loader.",
-//     email: "joedoe@test.com",
-//     phone: "64634634563465",
-//     status: "Available",
-//     willWorkIn: ["England", "Scotland", "Wales"],
-//     qualis: "",
-//     credits: [
-//       {
-//         jobTitle: "Some movie",
-//         role: "B Camera Loader",
-//       },
-//       {
-//         jobTitle: "Great Movie",
-//         role: "A Camera Loader",
-//       },
-//     ],
-//     bio: "Joe Doe is a loader. He is a very good loader.",
-//   },
-//   {
-//     id: 6,
-//     name: "Juandoe - Doublebarrelled",
-//     dept: "Grips",
-//     role: "Trainee",
-//     status: "SemiAvail",
-//     image:
-//       "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//     bio: "Juan Doe is a trainee. He is a very good trainee.",
-//     email: "juandoe@test.com",
-//     phone: "12345235345",
-//     status: "Not Available",
-//     willWorkIn: ["England", "Northern Ireland", "Scotland", "Wales", "Ireland"],
-//     qualis: "",
-//     credits: [
-//       {
-//         jobTitle: "Harry Potter",
-//         role: "B Camera Trainee",
-//       },
-//       {
-//         jobTitle: "Harry Potter 2",
-//         role: "A Camera Trainee",
-//       },
-//     ],
-//     bio: "Juan Doe is a trainee. He is a very good trainee.",
-//     cv: "/images/logoNew2.png",
-//   },
-// ];
+  const filter = (e) => {
+    const searchTerm = e.target.value;
+    console.log(searchTerm);
+    console.log(users);
+    if (searchTerm !== "") {
+      const results = users.filter((user) => {
+        return user.title.toLowerCase().startsWith(searchTerm.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFoundTitle(results);
+    } else {
+      setFoundTitle(users);
+      // If the text field is empty, show all users
+    }
+  };
 
-const crewList = ({users, image}) => {
+  const clearFilter = () => {
+    setFoundTitle(users);
+  }
 
   return (
     <div className="px-4 md:px-12 p-12 w-full h-fit">
@@ -216,7 +39,58 @@ const crewList = ({users, image}) => {
         </h1>
       </div>
 
+
       <div className="w-full flex justify-center">
+        <div className="flex w-full lg:max-w-[1200px] flex-col-reverse lg:flex-row gap-x-4 gap-y-4 mt-4">
+          {foundTitle && foundTitle.length > 0 ? (
+            <div className="flex flex-1 rounded-md flex-col gap-y-4 mb-12">
+              {foundTitle.map((user, i) => (
+                <CrewDetailBox
+                  key={user.id}
+                  id={user.id}
+                  name={user.username}
+                  dept={user.dept}
+                  title={user.title}
+                  canStepUp={user.canStepUp}
+                  imgURL={user.imgURL}
+                  phone={user.phone}
+                  email={user.email}
+                  website={user.website}
+                  status={user.status}
+                  willWorkIn={user.canWorkIn}
+                  qualis={user.qualis}
+                  credits={user.credits}
+                  bio={user.bio}
+                  cv={user.cv}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="mt-4 w-full text-center">
+              <h1>No user found!</h1>
+            </div>
+          )}
+          <div className=" w-full lg:w-3/12 shadow-md bg-white rounded-md p-4 lg:h-fit mb-4 md:mb-0">
+            <p className="text-center mb-4 font-semibold text-lg">
+              Filter by Role
+            </p>
+            <ul className="flex flex-wrap justify-center lg:justify-between lg:flex-col gap-y-4 gap-x-4 filterByList">
+              <li className="lg:w-full max-w-3/12">
+                <button className="w-full" onClick={clearFilter}>All</button>
+              </li>
+              {users.map((crew) => (
+                <li key={crew.id} className="lg:w-full max-w-3/12">
+                  <button className="w-full" value={crew.title} onClick={filter}>
+                    {crew.title}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="w-full flex justify-center">
         <div className="flex w-full lg:max-w-[1200px] flex-col-reverse lg:flex-row gap-x-4 gap-y-4 mt-4">
           <div className="flex flex-1 rounded-md flex-col gap-y-4 mb-12">
             {users.map((crew) => (
@@ -249,18 +123,22 @@ const crewList = ({users, image}) => {
               <li className="lg:w-full max-w-3/12">
                 <button className="w-full">All</button>
               </li>
-              <li className="lg:w-full max-w-3/12">
-                <button className="w-full">LIST OF ALL ROLES GOES HERE</button>
-              </li>
+              {users.map((crew) => (
+                <li key={crew.id} className="lg:w-full max-w-3/12">
+                  <button className="w-full" value={crew.title}>
+                    {crew.title}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export default crewList;
+export default CrewList;
 
 export async function getStaticPaths() {
   const { data: profiles } = await supabase.from("profiles").select("dept");
@@ -275,33 +153,28 @@ export async function getStaticPaths() {
   const paths = uniqueDepts.map((dept) => {
     return {
       params: {
-        crewList: dept
-      }
-    }
-  })
+        crewList: dept,
+      },
+    };
+  });
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-
 export const getStaticProps = async (context) => {
-  const dept = context.params.crewList
-  
-  const data = await supabase.from("profiles").select("*").eq("dept", dept);
-  
-// console.log("CREW DATA-", context.params.crewList);
-// console.log("PROFILES_", data.data.map((item) => item.imgURL));
+  const dept = context.params.crewList;
 
+  const data = await supabase.from("profiles").select("*").eq("dept", dept);
+
+  // console.log("CREW DATA-", context.params.crewList);
+  // console.log("PROFILES_", data.data.map((item) => item.imgURL));
 
   return {
     props: {
       users: data.data,
-    }
-  }
+    },
+  };
 };
-
-
-
