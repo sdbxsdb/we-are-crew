a0cc661f6f58b514d385da90ba05fb7f7b86a951
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-09-29 13:11:53 +0100

    remove red border

--- components/NavBar.js
+++ components/NavBar.js
@@ -23,7 +23,7 @@ const NavBar = ({req, res}) => {
   };
 
   return (
-    <div className="w-full borderRed top-0 fixed flex justify-center z-50 px-4 py-2 shadow-md bg-wearecrewLightGrey min-h-[105px]">
+    <div className="w-full top-0 fixed flex justify-center z-50 px-4 py-2 shadow-md bg-wearecrewLightGrey min-h-[105px]">
       <div className="max-w-[1200px] flex flex-col gap-y-2 md:gap-y-0 w-full">
         <nav className=" flex justify-between  items-center text-sm sm:text-base ">
           <Link href="/">
