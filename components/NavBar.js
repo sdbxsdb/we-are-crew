a0a0d80fd76351d715c65df0ed9c1de9d734945f
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-03 14:42:50 +0100

    profile button colour updated

--- components/NavBar.js
+++ components/NavBar.js
@@ -71,7 +71,7 @@ const NavBar = () => {
                 <span onClick={() => setShowProfileIconContent(!showProfileIconContent)} className={`material-icons cursor-pointer text-3xl pt-1 hover:text-wearecrewBlue transition ${showProfileIconContent ? 'text-wearecrewBlue' : ''}`}>account_circle</span>
                 {showProfileIconContent && (
 
-                    <div ref={dropDownRef} className="absolute w-max flex flex-col xl:left-[-2px] bg-white rounded-md shadow-md border-wearecrewOrange xl:transform xl:-translate-x-1/3 border-2 right-0">
+                    <div ref={dropDownRef} className="absolute w-max flex flex-col xl:left-[-2px] bg-white rounded-md shadow-md border-wearecrewBlue xl:transform xl:-translate-x-1/3 border-2 right-0">
                       <Link href="/my-crew" >
                         <a onClick={() => setShowProfileIconContent(false)} className="hover:bg-wearecrewLightGrey p-4">My Crew</a>
                       </Link>
