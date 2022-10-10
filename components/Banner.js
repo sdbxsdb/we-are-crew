7c61d7470b8a8cde34d8f28e12df0b60c7f0b11a
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-10 16:24:22 +0100

    working on about page

--- components/Banner.js
+++ components/Banner.js
@@ -4,7 +4,7 @@ import React from "react";
 const Banner = () => {
 
   return (
-    <div style={{ backgroundImage: `url("images/newBannerBlackfaded.png")`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="w-full flex justify-center overflow-hidden items-center h-[120px] md:h-[200px] relative px-4">
+    <div style={{ backgroundImage: `url("images/newBannerBlackfaded.png")`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="w-full flex justify-center overflow-hidden items-center h-[120px] md:h-[150px] relative px-4">
       <div className="flex relative flex-col items-center p-4 rounded-full">
         <p className="text-center text-1xl md:text-4xl text-white z-40 font-anton tracking-wide mb-5 md:mb-2">The UK &amp; Ireland’s <br />Film & Television Crew Database</p>
         <p className="text-lg text-white  bottom-0 md:bottom-12 z-50"><cite>The only place for crew.</cite></p>
--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,23 @@ import ShowDemoModal from "../components/ShowDemoModal";
 import Head from "next/head";
 import Link from "next/link";
 import Accordion from "../components/Accordion";
+import { useState } from "react";
 
-const about = () => {
+const About = () => {
+  const [showNeedCrew, setShowNeedCrew] = useState(true);
+  const [showAmCrew, setShowAmCrew] = useState(false);
+
+  const needCrewHandler = (e) => {
+    e.preventDefault();
+    setShowNeedCrew(true);
+    setShowAmCrew(false);
+  };
+  const amCrewHandler = (e) => {
+    e.preventDefault();
+    setShowAmCrew(true);
+    setShowNeedCrew(false);
+  };
+
   return (
     <>
       <Head>
@@ -16,7 +31,7 @@ const about = () => {
       </Head>
 
       <div className="aboutPage">
-        <div
+        {/* <div
           style={{
             backgroundImage: `url("images/newBannerBlackfaded.png")`,
             backgroundSize: "cover",
@@ -27,18 +42,208 @@ const about = () => {
           <h1 className="text-1xl text-center md:text-3xl tracking-wide">
             Any experience. Any department. Anyone.
           </h1>
-          {/* <img
-            src="/images/bannerBg4.png"
-            alt=""
-            width="100%"
-            height="100%"
-            className="absolute z-30 opacity-10 -top-[40px] md:-top-[110px]"
-          /> */}
-        </div>
+        </div> */}
 
         <div className="w-full flex justify-center mt-6 md:mt-12">
           <div className="flex flex-col items-center justify-center max-w-[1200px] gap-x-4 gap-y-4">
-            <div className="px-4 md:px-12 w-full flex flex-col md:flex-row gap-x-4 gap-y-4">
+            <div className="snap-y snap-mandatory h-[calc(100vh-105px)] overflow-scroll">
+              <div className="snap-start w-screen px-4 h-[calc(100vh-105px)] flex flex-col items-start justify-center ">
+                <div className="w-full flex justify-center gap-4 mb-12">
+                  <div className="flex gap-x-12 max-w-[1200px]">
+                    <div className="w-1/2">
+                      <h1 className="text-4xl">
+                        What is{" "}
+                        <span className="text-wearecrewBlue">Get Crew</span>{" "}
+                        <br /> and why do I need it?
+                      </h1>
+                    </div>
+                    <div className="w-1/2">
+                      <p>
+                        <span className="text-wearecrewBlue">Get Crew</span> has
+                        been designed and built with ALL crew in mind.
+                        <br />
+                        <br />
+                        Gone are the days of dated, regional crew listing pages
+                        hidden in the depths of a website.
+                        <br />
+                        <br />
+                        Create a profile and have it readily accessible to any
+                        Producer, Production Manager or Production needing crew
+                        in the UK or Ireland.
+                        <br />
+                        <br />
+                        The only place for crew.
+                        <br />
+                        Get Crew.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="snap-start w-screen px-4 h-[calc(100vh-105px)] flex flex-col items-start justify-center ">
+                <div className="w-full flex justify-center gap-4 mb-12">
+                  <div className="flex gap-x-12 max-w-[1200px]">
+                    <div className="w-1/2">
+                      <h1 className="text-4xl">The solution.</h1>
+                    </div>
+                    <div className="w-1/2">
+                      <p>
+                        <span className="text-wearecrewBlue">Get Crew</span> has
+                        been designed and built with ALL crew in mind.
+                        <br />
+                        <br />
+                        Gone are the days of dated, regional crew listing pages
+                        hidden in the depths of a website.
+                        <br />
+                        <br />
+                        Create a profile and have it readily accessible to any
+                        Producer, Production Manager or Production needing crew
+                        in the UK or Ireland.
+                        <br />
+                        <br />
+                        The only place for crew.
+                        <br />
+                        Get Crew.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="snap-start w-screen px-4 h-[calc(100vh-105px)] flex flex-col items-start justify-start pt-12 overflow-hidden">
+                <div className="w-full flex justify-center items-center gap-4 mb-12 ">
+                  <div className="w-full max-w-[1200px]">
+                    <div className="w-full text-center">
+                      <h1 className="text-4xl mb-4">How does it work?</h1>
+                    </div>
+                    <div className="w-full">
+                      <div className="flex flex-col items-center">
+                        
+                        <div className="w-full flex justify-center gap-x-4">
+                          <button
+                            onClick={(e) => needCrewHandler(e)}
+                            className={`
+                              ${
+                                showNeedCrew === true
+                                  ? "border-wearecrewBlue"
+                                  : "border-transparent"
+                              }
+                                break-normal largeBtn text-wearecrewDarkestGrey font-anton text-3xl lg:text-4xl text-center uppercase leading-relaxed border-b-[10px] border-wearecrewBlue shadow-md hoverScale w-full min-w-max md:w-[40%] p-4`}
+                                          >
+                                            <span className="min-w-max">I need crew</span>
+                          </button>
+
+                          <button
+                                            onClick={(e) => amCrewHandler(e)}
+                                            className={`
+                              ${
+                                showAmCrew === true
+                                  ? "border-wearecrewBlue"
+                                  : "border-transparent"
+                              }
+                              break-normal largeBtn text-wearecrewDarkestGrey font-anton text-3xl lg:text-4xl text-center uppercase leading-relaxed border-b-[10px] border-wearecrewBlue shadow-md hoverScale w-full  min-w-max md:w-[40%] p-4`}
+                                          >
+                            <span className="min-w-max">I am crew</span>
+                          </button>
+                        </div>
+
+                        <div className="relative h-full w-full">
+                          <div
+                            className={`
+                              ${
+                                showNeedCrew === true
+                                  ? "opacity-100 translate-x-[0%]"
+                                  : "opacity-0 translate-x-[100%]"
+                              } transform transition flex gap-x-4 gap-y-4 flex-col md:flex-row w-full md:justify-around items-start py-4 absolute mt-12`}>
+                            <div className="w-1/2">
+                              <h1 className="text-4xl leading-relaxed">
+                                The last place you&apos;ll need to look for
+                                crew.
+                              </h1>
+                              <div className="flex gap-4 items-center">
+                                <div className="my-8 w-1/2 flex flex-col gap-y-4 text-lg">
+                                  <p>
+                                    <strong>1.</strong> Pick a department.
+                                  </p>
+                                  <p>
+                                    <strong>2.</strong> Filter by role.
+                                  </p>
+                                  <p>
+                                    <strong>3.</strong> View the profile.
+                                  </p>
+                                </div>
+                                <div className="w-1/2">
+                                  <h1 className="text-center text-lg">
+                                    Free to search!
+                                  </h1>
+                                </div>
+                              </div>
+                              <div className="w-full flex justify-center mt-4">
+                                <Link href="/I-need-crew/depts">
+                                  <a className="bg-wearecrewBlue w-full text-center rounded-md shadow-md p-4 text-white">
+                                    Get Started
+                                  </a>
+                                </Link>
+                              </div>
+                            </div>
+                            <div className="w-1/2">
+                              <div className="h-[calc(100vh-400px)] overflow-y-scroll">
+                                <img src="/images/testIneedCrewImg.png" alt="" width="100%" height="200"/>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div
+                            className={`
+                  ${
+                    showAmCrew === true
+                      ? "opacity-100 translate-x-[0%]"
+                      : "opacity-0 translate-x-[100%]"
+                  } flex gap-x-4 transform transition gap-y-4 flex-col md:flex-row w-full md:justify-around items-start py-4 absolute top-0`}
+                          >
+                            <div className="w-full md:w-[40%]">
+                              <h1 className="text-2xl">I am Crew </h1>
+                              <div className="flex gap-4 items-center">
+                                <div className="my-2 w-1/2">
+                                  <p>
+                                    <strong>1.</strong> Pick a department.
+                                  </p>
+                                  <p>
+                                    <strong>2.</strong> Filter by role.
+                                  </p>
+                                  <p>
+                                    <strong>3.</strong> View the profile.
+                                  </p>
+                                </div>
+                                <div className="w-1/2">
+                                  <h1 className="text-center text-lg">
+                                    Free to search!
+                                  </h1>
+                                </div>
+                              </div>
+                              <div className="w-full flex justify-center mt-4">
+                                <Link href="/I-need-crew/depts">
+                                  <a className="bg-wearecrewBlue rounded-md shadow-md p-4 text-white">
+                                    Get Started
+                                  </a>
+                                </Link>
+                              </div>
+                            </div>
+                            <div className="bg-white rounded-md shadow-md w-full md:w-[40%]">
+                              <h1>Image</h1>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="snap-start w-screen h-[calc(100vh-105px)] flex items-center justify-center">
+                4
+              </div>
+            </div>
+
+            {/* <div className="px-4 md:px-12 w-full flex flex-col md:flex-row gap-x-4 gap-y-4">
               <Accordion title="Keeping it Green">
                 <p>
                   We all know the Film and Television industry isn’t exactly
@@ -96,15 +301,14 @@ const about = () => {
                   Get Crew.
                 </p>
               </Accordion>
-            </div>
-            <div className="px-4 md:px-12 flex flex-col md:flex-row gap-x-4 gap-y-4">
+            </div> */}
+            {/* <div className="px-4 md:px-12 flex flex-col md:flex-row gap-x-4 gap-y-4">
               <Accordion title="Two for the price of one.">
                 <p>
                   {" "}
                   If you want to list yourself as an individual and your company
                   or if you&apos;re an Accountant training to be a Stunt Person
-                  you can add another profile to your account at no extra
-                  cost!
+                  you can add another profile to your account at no extra cost!
                   <br />
                   <br />
                   Two profiles for the price of one.
@@ -241,15 +445,16 @@ const about = () => {
                   </a>
                 </p>
               </Accordion>
-            </div>
-            <div className="w-full flex gap-x-4 justify-center mt-12">
+            </div> */}
+
+            {/* <div className="w-full flex gap-x-4 justify-center mt-12">
               <Link href="/terms-and-conditions">
                 <small className="cursor-pointer">Terms & Conditions</small>
               </Link>
               <Link href="/privacy-policy">
                 <small className="cursor-pointer">Privacy Policy</small>
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -257,4 +462,4 @@ const about = () => {
   );
 };
 
-export default about;
+export default About;
