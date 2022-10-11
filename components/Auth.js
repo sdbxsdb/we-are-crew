29a9921a55337565b04e68425a6d14ea2742e658
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-11 15:55:27 +0100

    signin page updated

--- components/Auth.js
+++ components/Auth.js
@@ -59,59 +59,104 @@ export default function Auth() {
       <div className="">
         <Banner />
       </div>
-      <div className="flex item-center justify-center ">
-        <div className="flex justify-center items-center md:h-[calc(100vh-307px)] ">
-          <div className="flex flex-col items-center mt-12 md:mt-0 px-4">
-            <h1 className="mb-4 text-2xl text-center">
-              Enter your email to get a sign in / register link.
-            </h1>
-
-            <div className="bg-white rounded-md shadow-md flex flex-col items-center justify-center pt-4 pb-8">
-              <form className="w-full px-8 md:px-20 mb-12">
-                <div className="mb-2 rounded-md p-2 styledList w-full">
-                  <input
-                    className="inputField"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={handleEmailInput}
-                  />
-                  {showEmailError && (
-                    <p className="text-center text-wearecrewRed mt-1">
-                      Please enter a valid email
+      <div className="flex item-center justify-center">
+        <div className="flex justify-between w-full max-w-[1200px] items-center md:h-[calc(100vh-256px)] px-4">
+          <div className="flex flex-col-reverse md:flex-row w-full">
+            <div className="w-full mt-12 md:mt-4 md:w-4/12 flex flex-col items-start justify-center aboutPage">
+              <h1 className="text-3xl md:text-left text-center w-full mb-6">
+                Why sign up to{" "}
+                <span className="text-wearecrewBlue">Get Crew</span>?
+              </h1>
+              <div className="aboutPageListTicks">
+                <ul>
+                  <li>
+                    <p>
+                      Your profile will be readily accessible to any Production,
+                      Producer or PM needing crew in the UK or Ireland.
                     </p>
-                  )}
-                </div>
-                <div>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleLogin(email);
-                    }}
-                    className="p-2 w-full rounded-md border-2 border-wearecrewBlue mt-2"
-                    disabled={loading}
-                  >
-                    <span>{loading ? "Loading..." : "Send login link"}</span>
-                  </button>
-                </div>
-              </form>
-              <div className="px-12 w-full relative h-[50px]">
-                <hr className="" />
-                <small className="p-2 bg-white absolute -top-1/2 left-1/2 transform -translate-x-1/2 mt-2 opacity-70">
-                  Or you can
-                </small>
+                  </li>
+                  <li>
+                    <p>
+                      One place for all your professional credentials.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      List yourself as an individual and your company at no extra
+                      charge.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Change your own availability as and when you need to.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      1/10<sup>th</sup> of the cost compared to a traditional
+                      diary service’s ££££.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      No restrictions when it comes to your location, your
+                      experience or your department.
+                    </p>
+                  </li>
+                </ul>
               </div>
-              <div
-                onClick={() => signInWithGoogle()}
-                className="flex bg-white p-2 items-center gap-x-2 rounded-md shadow-md cursor-pointer border-2 border-wearecrewBlue w-fit"
-              >
-                <img
-                  src="/images/7123025_logo_google_g_icon.png"
-                  alt=""
-                  width="30px"
-                  height="30px"
-                />
-                <span className="">Sign in with Google</span>
+            </div>
+            <div className="flex flex-1 flex-col justify-center items-center mt-8 md:mt-0 px-4">
+              <h1 className="mb-4 text-lg text-center">
+                Enter your email <br/> to get a sign in / register link.
+              </h1>
+              <div className="bg-white w-full md:w-auto rounded-md shadow-md flex flex-col items-center justify-center pt-4 pb-8">
+                <form className="w-full px-8 md:px-20 mb-12 flex flex-col justify-center items-center">
+                  <div className="mb-2 rounded-md p-2 styledList">
+                    <input
+                      className="inputField"
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleEmailInput}
+                    />
+                    {showEmailError && (
+                      <p className="text-center text-wearecrewRed mt-1">
+                        Please enter a valid email
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleLogin(email);
+                      }}
+                      className="p-2 w-full rounded-md border-2 border-wearecrewBlue mt-2"
+                      disabled={loading}
+                    >
+                      <span>{loading ? "Loading..." : "Send login link"}</span>
+                    </button>
+                  </div>
+                </form>
+                <div className="px-12 w-full relative h-[50px]">
+                  <hr className="" />
+                  <small className="p-2 bg-white absolute -top-1/2 left-1/2 transform -translate-x-1/2 mt-2 opacity-70">
+                    Or you can
+                  </small>
+                </div>
+                <div
+                  onClick={() => signInWithGoogle()}
+                  className="flex bg-white p-2 items-center gap-x-2 rounded-md shadow-md cursor-pointer border-2 border-wearecrewBlue w-fit"
+                >
+                  <img
+                    src="/images/7123025_logo_google_g_icon.png"
+                    alt=""
+                    width="30px"
+                    height="30px"
+                  />
+                  <span className="">Sign in with Google</span>
+                </div>
               </div>
             </div>
           </div>
