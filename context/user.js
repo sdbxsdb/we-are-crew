b29da08872b9a6f99a82987f165536206ec5b38e
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-12 10:32:41 +0100

    working with user.js to try and update db live

--- context/user.js
+++ context/user.js
@@ -1,81 +1,76 @@
-import { createContext, useState, useEffect, useContext } from 'react';
-import { supabase } from '../utils/supabaseClient';
+import { createContext, useState, useEffect, useContext } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 const Context = createContext();
-
 
 const Provider = ({ children }) => {
   const [user, setUser] = useState(supabase.auth.getUser());
 
   useEffect(() => {
-
     const getUserProfile = async () => {
       const sessionUser = await supabase.auth.getUser();
 
       if (sessionUser) {
-        const {data: profile} = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", sessionUser?.data?.user?.id)
-        .single()
+        const { data: profile } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", sessionUser?.data?.user?.id)
+          .single();
 
         setUser({
           ...sessionUser,
           ...profile,
         });
-      };
+      }
     };
 
     getUserProfile();
 
     supabase.auth.onAuthStateChange(() => {
       getUserProfile();
-    })
+    });
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      supabase
+        .channel("public:profiles")
+        .on(
+          "postgres_changes",
+          { event: "*", schema: "public", table: "profiles" },
+          (payload) => {
+            // console.log("Change received!", payload);
+            setUser(payload.new);
+          }
+        )
+        .subscribe();
+    }
+    console.log("USER-", user);
   }, [user]);
-
-
-  // supabase
-  // .channel('*')
-  // .on('postgres_changes', { event: '*', schema: '*' }, payload => {
-  //   console.log('Change received!', payload)
-  //   setUser(payload);
-  //   console.log("USER-", user);
-  // })
-  // .subscribe()
- 
 
   // useEffect(() => {
   //   const getSession = async () => {
   //     const session = await supabase.auth.getSession();
-    
+
   //     console.log("SESSION-", session);
   //   }
   //   getSession();
   //   console.log("USER -", user.data);
   // }, [user])
 
-
   const logout = async () => {
     await supabase.auth.signOut();
     setUser(null);
   };
 
-  const exposed = { 
+  const exposed = {
     user,
     logout,
-  }
+  };
 
-
-  return (
-    <Context.Provider value={exposed}>
-      {children}
-    </Context.Provider>
-  )
-}
+  return <Context.Provider value={exposed}>{children}</Context.Provider>;
+};
 
 export const useUser = () => useContext(Context);
 
-
 export default Provider;
-
-
