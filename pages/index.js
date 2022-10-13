c3f106514040b649a7517c3e754cb88e8aea5ecb
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-13 12:39:50 +0100

    update home page i am crew button

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Banner from "../components/Banner";
 import LargeButton from "../components/LargeButton";
+import {user} from '../context/user';
 
 
 export default function Home() {
@@ -22,7 +23,7 @@ export default function Home() {
       <div className="mt-2 md:mt-0 h-[calc(100vh-305px)] flex justify-center w-full">
         <div className="flex gap-x-4 gap-y-4 flex-col md:flex-row w-full md:justify-between items-center px-4 md:px-12 md:py-12 max-w-[1200px]">
           <LargeButton text="I need crew" link="/I-need-crew/depts" />
-          <LargeButton text="I am crew" link="/my-crew" />
+          <LargeButton text={`${user ? "My Profile" : "I am Crew"}`} link="/my-crew" />
         </div>
       </div>
 
