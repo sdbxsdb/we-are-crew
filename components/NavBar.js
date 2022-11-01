3fc8e9a336676efd19e548c694217fb8090966fb
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-01 22:17:26 +0000

    update logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -50,7 +50,7 @@ const NavBar = () => {
           <Link href="/">
             <a className="h-full w-[140px] relative flex justify-center items-center hover:brightness-110 transition">
               <img
-                src="/images/newGCLogoWLogo.png"
+                src="/images/newGcLogoWLogo2.png"
                 className="z-50"
                 alt="Get Crew logo"
               />
