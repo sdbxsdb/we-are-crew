d7c7148980e26b839d87c1342dfa37fbea7f8c3e
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-01 21:10:48 +0000

    new banner text

--- components/Banner.js
+++ components/Banner.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-
 const Banner = () => {
-
   return (
-    <div style={{ backgroundImage: `url("images/newBannerBlackfaded.png")`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="w-full flex justify-center overflow-hidden items-center h-[120px] md:h-auto relative px-4">
-      <div className="flex relative flex-col items-center p-4 rounded-full">
-        <p className="text-center text-1xl md:text-4xl text-white z-40 font-anton tracking-wide mb-5 md:mb-2 shadow-md">The UK &amp; Ireland’s <br /> Film & Television Crew Hub</p>
-        <p className="text-lg text-white  bottom-0 md:bottom-12 z-50"><cite>The last place you&apos;ll need.</cite></p>
+    <div
+      style={{
+        backgroundImage: `url("images/newBannerBlackfaded.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="w-full flex justify-center overflow-hidden items-center h-[120px] md:h-auto relative px-4"
+    >
+      <div className="flex relative flex-col items-center px-4 py-6 rounded-full">
+        <div className="text-center text-white z-40 font-anton tracking-wide mt-4 md:mt-0 mb-0 md:mb-2 shadow-md">
+          <span className="text-2xl md:text-2xl">The only place for crew <br /></span>
+          <span className="text-2xl md:text-4xl">UK & Ireland</span>
+          
+        </div>
+        {/* <p className="text-lg text-white  bottom-0 md:bottom-12 z-50"><cite>The last place you&apos;ll need.</cite></p> */}
       </div>
       {/* <img src="/images/newBanner.png" alt="" width="100%" height="100%" className="absolute z-30 opacity-100 -top-[40px] md:-top-[110px]" /> */}
     </div>
