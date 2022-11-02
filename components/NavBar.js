c80b3d8864d9b57f1cfc05aafaa0a55c0c526a0e
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-02 21:14:51 +0000

    revert to working nav?

--- components/NavBar.js
+++ components/NavBar.js
@@ -118,6 +118,124 @@ const NavBar = () => {
               </NavLink>
             )}
             {userEmail && (
+              <>
+                <NavLink href="/my-crew">
+                  <h1
+                    onClick={() => setShowProfileIconContent(false)}
+                    className="text-wearecrewBlue p-4"
+                  >
+                    Profile
+                  </h1>
+                </NavLink>
+                <button
+                  onClick={() => signOutHandler()}
+                >
+                  <h1 className="text-wearecrewBlue">Logout</h1>
+                </button>
+              </>
+            )}
+          </div>
+          {/* END OF // DESKTOP NAV */}
+
+          {/* MOBILE NAV */}
+
+          <div
+            onClick={() => showSlideoverNav()}
+            className="cursor-pointer px-5 py-2 rounded "
+          >
+            <span className="material-icons text-4xl text-wearecrewDarkestGrey">
+              menu
+            </span>
+          </div>
+          <div
+            className={`w-screen h-screen flex items-center text-6xl justify-center flex-col gap-y-8 pb-24 px-4 left-0 top-0 bg-wearecrewDarkestGrey/95 fixed text-wearecrewLightGrey transition transform ${
+              showMobileNav === true
+                ? "-translate-x-[0px]"
+                : "translate-x-[500px]"
+            }`}
+          >
+            <button
+              onClick={hideSlideoverNav}
+              className="absolute z-5000 top-5 right-8"
+            >
+              <svg
+                className="transition fill-current hover:text-wearecrewDarkBlue"
+                width="30px"
+                height="30px"
+                x="0px"
+                y="0px"
+                viewBox="0 0 252 252"
+              >
+                <g>
+                  <path
+                    d="M126,0C56.523,0,0,56.523,0,126s56.523,126,126,126s126-56.523,126-126S195.477,0,126,0z M126,234
+              c-59.551,0-108-48.449-108-108S66.449,18,126,18s108,48.449,108,108S185.551,234,126,234z"
+                  />
+                  <path
+                    d="M164.612,87.388c-3.515-3.515-9.213-3.515-12.728,0L126,113.272l-25.885-25.885c-3.515-3.515-9.213-3.515-12.728,0
+              c-3.515,3.515-3.515,9.213,0,12.728L113.272,126l-25.885,25.885c-3.515,3.515-3.515,9.213,0,12.728
+              c1.757,1.757,4.061,2.636,6.364,2.636s4.606-0.879,6.364-2.636L126,138.728l25.885,25.885c1.757,1.757,4.061,2.636,6.364,2.636
+              s4.606-0.879,6.364-2.636c3.515-3.515,3.515-9.213,0-12.728L138.728,126l25.885-25.885
+              C168.127,96.601,168.127,90.902,164.612,87.388z"
+                  />
+                </g>
+              </svg>
+            </button>
+            <NavLink href="/">
+              <h1
+                onClick={hideSlideoverNav}
+                className={
+                  router.pathname == "/"
+                    ? "text-wearecrewBlue"
+                    : "hover:text-wearecrewBlue transition cursor-pointer"
+                }
+              >
+                Home
+              </h1>
+            </NavLink>
+            <NavLink href="/about">
+              <h1
+                onClick={hideSlideoverNav}
+                className={
+                  router.pathname == "/about"
+                    ? "text-wearecrewBlue"
+                    : "hover:text-wearecrewBlue transition cursor-pointer"
+                }
+              >
+                About
+              </h1>
+            </NavLink>
+
+            <NavLink href="/pricing">
+              <h1
+                onClick={hideSlideoverNav}
+                className={
+                  router.pathname == "/pricing"
+                    ? "text-wearecrewBlue"
+                    : "hover:text-wearecrewBlue transition cursor-pointer"
+                }
+              >
+                Pricing
+              </h1>
+            </NavLink>
+
+            {!userEmail && (
+              <NavLink href="/my-crew">
+                <h1
+                  onClick={hideSlideoverNav}
+                  className={`text-center
+                      ${
+                        router.pathname == "/my-crew"
+                          ? "text-wearecrewBlue"
+                          : "hover:text-wearecrewBlue transition cursor-pointer"
+                      }
+                    `}
+                >
+                  Sign In / Register
+                </h1>
+              </NavLink>
+            )}
+            {userEmail && (
               <div className="relative">
                 <span
                   onClick={() =>
@@ -136,7 +254,7 @@ const NavBar = () => {
                   >
                     <Link href="/my-crew">
                       <a
-                        onClick={() => setShowProfileIconContent(false)}
+                        onClick={hideSlideoverNav}
                         className="hover:bg-wearecrewLightGrey p-4"
                       >
                         Profile
@@ -152,123 +270,6 @@ const NavBar = () => {
                 )}
               </div>
             )}
-          </div>
-          {/* END OF // DESKTOP NAV */}
-
-          {/* MOBILE NAV */}
-
-          <div className="block md:hidden">
-            <div
-              onClick={() => showSlideoverNav()}
-              className="cursor-pointer px-5 py-2 rounded "
-            >
-              <span className="material-icons text-4xl text-wearecrewDarkestGrey">
-                menu
-              </span>
-            </div>
-            <div
-              className={`w-screen h-screen flex items-center text-6xl justify-center flex-col gap-y-8 pb-24 px-4 left-0 top-0 bg-wearecrewDarkestGrey/95 fixed text-wearecrewLightGrey transition transform ${
-                showMobileNav === true
-                  ? "-translate-x-[0px]"
-                  : "translate-x-[770px]"
-              }`}
-            >
-              <button
-                onClick={hideSlideoverNav}
-                className="absolute z-5000 top-5 right-8"
-              >
-                <svg
-                  className="transition fill-current hover:text-wearecrewDarkBlue"
-                  width="30px"
-                  height="30px"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 252 252"
-                >
-                  <g>
-                    <path
-                      d="M126,0C56.523,0,0,56.523,0,126s56.523,126,126,126s126-56.523,126-126S195.477,0,126,0z M126,234
-                c-59.551,0-108-48.449-108-108S66.449,18,126,18s108,48.449,108,108S185.551,234,126,234z"
-                    />
-                    <path
-                      d="M164.612,87.388c-3.515-3.515-9.213-3.515-12.728,0L126,113.272l-25.885-25.885c-3.515-3.515-9.213-3.515-12.728,0
-                c-3.515,3.515-3.515,9.213,0,12.728L113.272,126l-25.885,25.885c-3.515,3.515-3.515,9.213,0,12.728
-                c1.757,1.757,4.061,2.636,6.364,2.636s4.606-0.879,6.364-2.636L126,138.728l25.885,25.885c1.757,1.757,4.061,2.636,6.364,2.636
-                s4.606-0.879,6.364-2.636c3.515-3.515,3.515-9.213,0-12.728L138.728,126l25.885-25.885
-                C168.127,96.601,168.127,90.902,164.612,87.388z"
-                    />
-                  </g>
-                </svg>
-              </button>
-              <NavLink href="/">
-                <h1
-                  onClick={hideSlideoverNav}
-                  className={
-                    router.pathname == "/"
-                      ? "text-wearecrewBlue"
-                      : "hover:text-wearecrewBlue transition cursor-pointer"
-                  }
-                >
-                  Home
-                </h1>
-              </NavLink>
-              <NavLink href="/about">
-                <h1
-                  onClick={hideSlideoverNav}
-                  className={
-                    router.pathname == "/about"
-                      ? "text-wearecrewBlue"
-                      : "hover:text-wearecrewBlue transition cursor-pointer"
-                  }
-                >
-                  About
-                </h1>
-              </NavLink>
-              <NavLink href="/pricing">
-                <h1
-                  onClick={hideSlideoverNav}
-                  className={
-                    router.pathname == "/pricing"
-                      ? "text-wearecrewBlue"
-                      : "hover:text-wearecrewBlue transition cursor-pointer"
-                  }
-                >
-                  Pricing
-                </h1>
-              </NavLink>
-              {!userEmail && (
-                <NavLink href="/my-crew">
-                  <h1
-                    onClick={hideSlideoverNav}
-                    className={`text-center
-                        ${
-                          router.pathname == "/my-crew"
-                            ? "text-wearecrewBlue"
-                            : "hover:text-wearecrewBlue transition cursor-pointer"
-                        }
-                      `}
-                  >
-                    Sign In / Register
-                  </h1>
-                </NavLink>
-              )}
-              {userEmail && (
-                <>
-                  <NavLink href="/my-crew">
-                    <h1
-                      onClick={hideSlideoverNav}
-                      className="bg-wearecrewBlue cursor-pointer"
-                    >
-                      Profile
-                    </h1>
-                  </NavLink>
-            
-                    <NavLink onClick={() => signOutHandler()}>
-                      <h1 className="bg-wearecrewBlue cursor-pointer">Logout</h1>
-                    </NavLink>
-                </>
-              )}
-            </div>
           </div>
 
           {/* END OF // MOBILE NAV */}
