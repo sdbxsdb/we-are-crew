ec4531ffdc203a91a6679ab0f952a0d4d5830fbd
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-16 12:19:44 +0000

    carousel added to home page and before installing material tailwind

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,14 @@ import Banner from "../components/Banner";
 import LargeButton from "../components/LargeButton";
 import { useUser } from "../context/user";
 import Link from "next/link";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 export default function Home() {
   const { user } = useUser();
@@ -21,7 +29,7 @@ export default function Home() {
         <div className="w-full hidden md:flex items-center justify-center absolute mt-12 font-bold text-2xl text-center  "></div>
       </div>
 
-      <div className="mt-8 md:mt-0 h-auto md:h-[calc(100vh-205px)] flex flex-col justify-center items-center w-full border-2 border-blue-400 overflow-scroll">
+      <div className="mt-8 md:mt-0 h-auto md:h-[calc(100vh-205px)] flex flex-col justify-center items-center w-full overflow-scroll">
         <div className="hidden md:flex flex-col gap-x-8 gap-y-4 px-4 w-full text-2xl font-bold justify-around max-w-[1200px] text-center ">
           {/* <hr className="w-7/12 m-auto" /> */}
           <cite className="py-4">
@@ -42,9 +50,72 @@ export default function Home() {
             link="/my-crew"
           />
         </div>
-        <div className="w-1/2  max-w-[1200px] flex flex-col ">
+
+        <div className="w-full  max-w-[1200px] flex flex-col ">
           <hr className="w-7/12 m-auto" />
-          <div className="slider my-2">
+
+          <CarouselProvider
+            className="py-4"
+            naturalSlideWidth={100}
+            naturalSlideHeight={125}
+            totalSlides={3}
+            isPlaying={true}
+            interval={5000}
+            dragStep={1}
+            infinite={true}
+            isIntrinsicHeight={true}
+          >
+            <Slider>
+              <Slide index={0} className="">
+                <div className="text-center flex flex-col justify-center h-[100px]">
+                  <cite className="flex">
+                    <div className="w-full">
+                      <span className="font-bold mr-2 text-2xl">&#34;</span>I
+                      welcome the launch of{" "}
+                      <span className="text-wearecrewBlue">Get Crew</span> to
+                      market as in this fast paced / time poor world this
+                      platform provides <br /> the perfect solution to seeking
+                      out crew for short and long term projects.
+                      <span className="font-bold ml-2 text-2xl">&#34;</span>
+                    </div>
+                  </cite>
+                  <p className="font-bold mt-4">
+                    {" "}
+                    - Val Croft | Head of Production Frank Films
+                  </p>
+                </div>
+              </Slide>
+              <Slide index={1}>
+                <div className="text-center flex flex-col justify-center h-[120px]">
+                  <cite className="flex">
+                    <div className="w-full">
+                      <span className="font-bold mr-2 text-2xl">&#34;</span>This
+                      is what the industry has been missing! So easy I&apos;ve
+                      been able to find and contact crew within 10sec of coming
+                      to the site. <br /> This will be my first port of call
+                      from now on.
+                      <span className="font-bold ml-2 text-2xl">&#34;</span>
+                    </div>
+                  </cite>
+                  <p className="font-bold mt-4">
+                    {" "}
+                    - Person 1 | Head of Production Frank Films
+                  </p>
+                </div>
+              </Slide>
+              <Slide index={2}>
+                <div className="text-center flex flex-col justify-center h-[100px]">
+                  <cite>&#34; Test text test testtest test</cite>
+                  <p className="font-bold mt-4">
+                    {" "}
+                    - Person 2 | Head of Production Frank Films
+                  </p>
+                </div>
+              </Slide>
+            </Slider>
+          </CarouselProvider>
+
+          {/* <div className="slider my-2">
             <Link href="#slide-1" scroll={false}>
               <a>1</a>
             </Link>
@@ -92,7 +163,7 @@ export default function Home() {
               <div id="slide-4">4</div>
               <div id="slide-5">5</div>
             </div>
-          </div>
+          </div> */}
           <hr className="w-7/12 m-auto" />
         </div>
       </div>
