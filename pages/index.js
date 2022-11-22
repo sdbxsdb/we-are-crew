3ed720842ff656ef30eb9a18469b3bfad3ccbdb2
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-22 19:05:57 +0000

    remove last

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,7 @@ export default function Home() {
     }
   }, [user]);
 
-  useEffect(() => {
-    if (user?.paid === true) {
-      setShowFinishModal(false);
-    } else if (user?.username) {
-      setShowFinishModal(false);
-    } 
-  }, [user]);
+
 
   return (
     <>
