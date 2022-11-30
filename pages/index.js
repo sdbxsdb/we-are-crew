4dd2b6900710b8b7090abdb4e6f5ad0a868ac5d5
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-11-30 21:07:27 +0000

    styling on new home modal

--- pages/index.js
+++ pages/index.js
@@ -150,7 +150,7 @@ export default function Home() {
                       </Link>
                     </strong>
                   </div>
-                  <div className="neumorphBoxMd cursor-pointer transform transition hover:scale-102 w-min-max text-center rounded-md group px-4 py-2 text-2xl">
+                  <div className="neumorphBoxMd cursor-pointer transform transition hover:scale-102 mt-4 w-min-max text-center rounded-md group px-4 py-2 text-2xl">
                     <Link href="/I-need-crew/depts">
                       <a className=" w-full text-center group-hover:opacity-70 transition text-wearecrewBlue">
                         <h1>Finish setting up your profile</h1>
