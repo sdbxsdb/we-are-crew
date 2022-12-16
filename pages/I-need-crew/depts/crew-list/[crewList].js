8b427a491dc51bf8fd55460a693bc2f3a5ac84fe
Sam Donaghy-Bell <samdonaghybell@gmail.com>
2022-12-16 22:07:42 +0000

    trying [crewList] again

--- pages/I-need-crew/depts/crew-list/[crewList].js
+++ pages/I-need-crew/depts/crew-list/[crewList].js
@@ -757,13 +757,11 @@ export async function getStaticPaths() {
   const uniqueDepts = Array.from(new Set(depts));
 
   const paths = uniqueDepts?.map((dept) => {
-    if (dept !== null) {
-      return {
-        params: {
-          crewList: dept,
-        },
-      };
-    }
+    return {
+      params: {
+        crewList: dept,
+      },
+    };
   });
 
   return {
